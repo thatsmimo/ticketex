ef3000afbaeee8f0425157e2a7f903da55886410
Mainak Shil <mainak@Mainaks-MacBook-Pro.local>
2020-10-30 20:39:06 +0530

    stable branch

--- src/screens/AddTicket/AddTicketScreen.js
+++ src/screens/AddTicket/AddTicketScreen.js
@@ -1,107 +1,158 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  ScrollView,
-} from "react-native";
-import { Modal, RadioButton } from "react-native-paper";
+import { View, Text, TextInput } from "react-native";
 import styles from "./styles";
-import { AppHeader, AppButton, SearchablePicker } from "../../components";
+import { Picker } from "@react-native-community/picker";
+import { AppHeader, AppButton, AppEditText } from "../../components";
 import { Colors, CommonStyles, Languages } from "../../js/common";
-import { Ionicons } from "@expo/vector-icons";
-import IconDir from "../../js/common/IconDir";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const AddTicketScreen = ({ navigation }) => {
   const insets = useSafeAreaInsets();
-  const [primaryDropdown, setPrimaryDropDown] = useState("Set Category");
-  const [options, setOptions] = useState([]);
 
-  const [visible, setVisible] = useState(false);
-  const showModal = () => setVisible(true);
-  const hideModal = () => setVisible(false);
-  const [value, setValue] = useState("");
+  const [selectedFirst, setSelectedFirst] = useState(false);
+  const [selectedSecond, setSelectedSecond] = useState(false);
+  const [selectedThird, setSelectedThird] = useState(false);
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("----");
+  const [eventType, setEventType] = useState("----");
+  const [classType, setClassType] = useState("----");
 
-  const getOptions = async () => {
-    const response = await Api.get("category/list");
-    console.log("res: ", response);
-    if (response.status) {
-      setOptions(response.categories);
-      // setOptions(dummyData);
+  const _handleNavigate = () => {
+    navigation.navigate("AddImage");
+  };
+
+  const EventType = () => {
+    if (selectedFirst) {
+      return (
+        <View style={styles.form}>
+          <Text>Class Type</Text>
+          <View style={styles.pickerBG}>
+            <Picker
+              selectedValue={classType}
+              style={styles.picker}
+              onValueChange={(itemValue, itemIndex) => {
+                setClassType(itemValue);
+                setSelectedSecond(true);
+              }}
+            >
+              <Picker.Item label="----" value="default" />
+              <Picker.Item label="Singles-Section c" value="java" />
+            </Picker>
+          </View>
+        </View>
+      );
+    } else {
+      return <></>;
     }
   };
 
-  const ModalOptions = () => {
-    return (
-      <Modal visible={visible} onDismiss={hideModal}>
-        <View style={styles.modalView}>
-          <ScrollView>
-            <RadioButton.Group
-              onValueChange={(value) => {
-                setPrimaryDropDown(
-                  value === " " ? "Select Category" : options[value - 1].name
-                );
-                setValue(value);
-                setVisible(false);
-                search(value);
-              }}
-              value={value}
-            >
-              <RadioButton.Item
-                label="Set Category"
-                value={" "}
-                style={{ width: "100%" }}
-                labelStyle={{ width: "80%" }}
-              />
-              {options.map((element, index) => (
-                <RadioButton.Item
-                  label={element.name}
-                  value={index + 1}
-                  key={index}
-                  style={{ width: "100%" }}
-                  labelStyle={{ width: "80%" }}
+  const Quantity = () => {
+    if (selectedSecond) {
+      return (
+        <>
+          <View style={styles.form}>
+            <Text>Quantity</Text>
+            <View style={styles.pickerBG}>
+              <Picker
+                selectedValue={quantity}
+                style={styles.picker}
+                mode="dialog"
+                // prompt="ss"
+                onValueChange={(itemValue, itemIndex) => {
+                  setQuantity(itemValue);
+                  setSelectedThird(true);
+                }}
+                itemStyle={{ fontFamily: "regular", fontSize: 11 }}
+              >
+                <Picker.Item label="----" value="default" />
+                <Picker.Item label="1" value="java" />
+              </Picker>
+            </View>
+          </View>
+        </>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
+  const PricePerTicket = () => {
+    if (selectedThird) {
+      return (
+        <>
+          <View style={styles.form}>
+            <View style={styles.input}>
+              <Text>Price per Ticket</Text>
+              {/* <AppEditText
+                value={price}
+                keyBoardType={"number-pad"}
+                saveText={(text) => setPrice(() => text)}
+              /> */}
+              <View
+                style={{
+                  height: 45,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  borderRadius: 45 / 2,
+                  paddingLeft: 5,
+                  marginTop: 15,
+                  borderColor: Colors.lineColor,
+                  borderWidth: 0.5,
+                }}
+              >
+                <TextInput
+                  style={{
+                    fontSize: 13,
+                    fontFamily: "regular",
+                    color: "#1d1d1d",
+                    paddingHorizontal: 10,
+                    flex: 1,
+                    justifyContent: "center",
+                  }}
+                  saveText={(text) => setPrice(() => text)}
                 />
-              ))}
-            </RadioButton.Group>
-          </ScrollView>
-        </View>
-      </Modal>
-    );
+              </View>
+            </View>
+          </View>
+          <View style={styles.btn}>
+            <AppButton
+              name={Languages.Next}
+              _handleOnPress={_handleNavigate}
+              disabled={price === "" ? true : false}
+            />
+          </View>
+          <Text style={styles.text}>
+            Note: Based on ticket class the price should not exceed 240 SAR
+          </Text>
+        </>
+      );
+    } else {
+      return <View />;
+    }
   };
 
   return (
     <View style={CommonStyles.screensRootContainer(insets.top)}>
       <AppHeader title="Add Ticket" />
-      <View style={{ marginHorizontal: "10%" }}>
-        <TouchableOpacity onPress={showModal}>
-          <View style={{ flexDirection: "row", marginTop: 15 }}>
-            <View
-              style={{
-                backgroundColor: "#EEF7FF",
-                borderRadius: 20,
-                flex: 1,
-                flexDirection: "row",
-                padding: 13,
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: "semi",
-                  flex: 1,
-                  color: Colors.lineColor,
-                }}
-              >
-                {primaryDropdown}
-              </Text>
-              <Ionicons name={IconDir.Ionicons.down} size={20} />
-            </View>
-            {/* <Separator width={10} /> */}
-          </View>
-        </TouchableOpacity>
+      <View style={styles.form}>
+        <Text>Event Type</Text>
+        <View style={styles.pickerBG}>
+          <Picker
+            selectedValue={eventType}
+            style={styles.picker}
+            onValueChange={(itemValue, itemIndex) => {
+              setEventType(itemValue);
+              setSelectedFirst(true);
+            }}
+          >
+            <Picker.Item label="----" value="default" />
+            <Picker.Item label="Alhila vs Alnaseer" value="java" />
+          </Picker>
+        </View>
       </View>
-      <ModalOptions />
+      <EventType />
+      <Quantity />
+      <PricePerTicket />
     </View>
   );
 };
